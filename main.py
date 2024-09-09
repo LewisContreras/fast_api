942fa82932d6074fcace4e9f0f126d05dd5afe8f
Lewis Contreras <legiconba4@gmail.com>
2024-09-08 21:49:18 -0500

    Initial commit for fastapi project

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+app.title = "My app with FastAPI"
+app.version = "0.0.1"
+
+@app.get("/", tags=["home"])
+def message():
+    return {"message": "Hello World"}
