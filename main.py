57fd0aec57d0a1b8a62a7ca1f405564c6628c082
Lewis Contreras <legiconba4@gmail.com>
2024-09-08 22:29:19 -0500

    Implementing get method

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+    {
+        "id": 2,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que...",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Acción"
+    },
+]
 
 app.title = "My app with FastAPI"
 app.version = "0.0.1"
 
 @app.get("/", tags=["home"])
 def message():
-    return {"message": "Hello World"}
+    return HTMLResponse("<h1>Hello, World!</h1>")
+
+@app.get("/movies", tags=["movies"])
+def get_movies():
+    return movies
+
+@app.get("/movies/{movie_id}", tags=["movies"])
+def get_movie(movie_id: int):
+    for movie in movies:
+        if movie["id"] == movie_id:
+            return movie
+    return {"error": "Movie not found"}
+
+@app.get("/movies/", tags=["movies"])
+def get_movies_by_category(category: str):
+    movies_by_category = []
+    for movie in movies:
+        if movie["category"] == category:
+            movies_by_category.append(movie)
+    return movies_by_category
